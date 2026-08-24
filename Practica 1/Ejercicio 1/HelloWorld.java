1acea3858fbf48ae2959f33ebf54338ee749a4a7
diegoLara666 <diegolara2007jim@hotmail.com>
2026-08-24 09:01:02 -0600

    Add files via upload

---
+++ Practica 1/Ejercicio 1/HelloWorld.java
@@ -0,0 +1,13 @@
+//Diego Armando Lara Jimenez
+
+
+public class HelloWorld {
+
+ 
+    public static void main(String[] args) {
+       System.out.println("Hola Mundo");
+        
+        
+    }
+    
+}
