4b8816a3c88387b870bc34aff13a2ece472a1100
diegoLara666 <diegolara2007jim@hotmail.com>
2026-08-24 09:01:28 -0600

    Add files via upload

---
+++ Practica 1/Ejercicio 2/CustomHelloWorld.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+//Lara Jimenez Diego Armando
+public class CustomHelloWorld {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Escribe tu nombre: ");
+        String nombre = scanner.nextLine();
+
+
+        System.out.println("Hola, " + nombre );
+
+  
+        scanner.close();
+    }
+}
