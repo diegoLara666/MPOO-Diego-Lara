24130c9929a7959db05f071cbc9eeeec51e359ae
diegoLara666 <diegolara2007jim@hotmail.com>
2026-08-24 09:02:13 -0600

    Add files via upload

---
+++ Practica 1/Ejercicio 3/ProgramaConErrores.java
@@ -0,0 +1,11 @@
+/**
+Autor: Lara Jimenez Diego Armando
+El programa debe mostrar tres mensajes en pantalla.
+**/
+public class ProgramaConErrores {
+    public static void main(String[] args) {
+        System.out.println("Estoy aprendiendo Java");
+        System.out.println("El compilador puede ayudarme");
+        System.out.println("Voy a encontrar todos los errores.");
+    }
+}
